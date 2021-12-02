6ecf9f0825045b7a23539b7f42afba1ea99ce981
BuildTools <unconfigured@null.spigotmc.org>
2021-12-02 13:59:48 -0500

    first cut of seemingly working find_next

--- src/modules/suffix_tree.js
+++ src/modules/suffix_tree.js
@@ -9,6 +9,15 @@ export class SuffixTree {
         this.fprime = [undefined, 0];
         this.t = [];
         this.final = [];
+        this.token_lengths = [];
+        this.final_states = [];
+        this.string_lengths = [];
+    }
+
+    add_token( length ) {
+        var token = this.token_lengths.length;
+        this.token_lengths[token] = length;
+        return token;
     }
 
     new_node() {
@@ -16,55 +25,59 @@ export class SuffixTree {
     }
 
     set_transition( s, k, l, r ) {
-        if( !this.gprime.hasOwnProperty( s ) )
+        if( !this.gprime.hasOwnProperty( s ) ) {
             this.gprime[s] = new Map();
+        }
         this.gprime[s][this.t[k]] = [k, l, r];
     }
 
     get_transition( s, t ) {
-        if( this.gprime.hasOwnProperty(s) )
+        if( this.gprime.hasOwnProperty(s) ) {
             return this.gprime[s][t];
-        else if( s == 0 ) {
+        } else if( s == 0 ) {
             return [-t,-t,1];
-        } else
+        } else {
             return undefined;
+        }
     }
 
-    update( s, k, i, final_state ) {
+    update( s, k, i ) {
         var oldr = this.root;
         var [end_point, r] = this.test_and_split( s, k, i-1, this.t[i] );
-        if) !end_point )
-            final_state = r;
         while( !end_point ) {
             var rprime = this.new_node();
             this.set_transition( r, i, infinity, rprime );
-            if( oldr != this.root )
+            if( oldr != this.root ) {
                 this.fprime[oldr] = r;
+            }
             oldr = r;
             [s,k] = this.cannonize( this.fprime[s], k, i-1 );
             [end_point, r] = this.test_and_split( s, k, i-1, this.t[i] );
         }
-        if( oldr != r )
+        if( oldr != r ) {
             this.fprime[oldr] = s;
-        return [s, k, final_state]
+        }
+        return [s, k]
     }
 
     test_and_split( s, k, p, t ) {
         if( k <= p ) {
             var [kprime, pprime, sprime] = this.get_transition( s, this.t[k] );
-            if( t == this.t[kprime + p - k + 1] )
+            if( t == this.t[kprime + p - k + 1] ) {
                 return [true, s];
-            else {
+            } else {
                 var r = this.new_node();
                 this.set_transition( s, kprime, kprime + p - k, r );
                 this.set_transition( r, kprime + p - k + 1, pprime, sprime );
                 return [false, r];
             }
-        } else
-            if( this.get_transition( s, t ) == undefined )
+        } else {
+            if( this.get_transition( s, t ) == undefined ) {
                 return [false, s];
-            else
+            } else {
                 return [true, s];
+            }
+        }
     }
 
     cannonize( s, k, p ) {
@@ -75,26 +88,85 @@ export class SuffixTree {
             while( pprime - kprime  <= p - k ) {
                 k = k + pprime - kprime + 1;
                 s = sprime;
-                if( k <= p )
+                if( k <= p ) {
                     [kprime, pprime, sprime] = this.get_transition( s, this.t[k] );
+                }
             }
             return [s,k];
         }
     }
 
-    concat( t ) {
-        var s = 1;
+    push( t ) {
+        var s = this.root;
         var k = this.t.length;
         var i = k-1;
-        t.push( 0 );
+
+        // process tokens
+        for( var j = 0; j < t.length; j++ ) {
+            if( this.token_lengths.hasOwnProperty( t[j] ) ) {
+                if( this.token_lengths[t[j]] != 1 ) {
+                    alert( "Invalid token!" );
+                }
+            } else {
+                this.token_lengths[t[j]] = 1;
+            }
+        }
+        var string_num = this.add_token( t.length );
+        this.string_lengths[string_num] = t.length;
+        t.push( string_num );
+
+        // append to the tree
         this.t = this.t.concat( t );
-        final_state = 1;
-        while( this.t[i+1] != 0 ) {
+        while( i < this.t.length-1 ) {
             i += 1;
-            [s, k, final_state] = this.update( s, k, i, final_state );
+            [s, k] = this.update( s, k, i );
             [s, k] = this.cannonize( s, k, i );
         }
+
+        s = this.root;
+        k = this.t.length - t.length;
+        i = k-1;
+        var r, p;
+        [k, p, r] = this.get_transition( s, this.t[i+1] );
+        while( p < infinity ) {
+            s = r;
+            i += p - k + 1;
+            [k, p, r] = this.get_transition( s, this.t[i+1] );
+        }
+        if( this.t[k] != string_num ) {
+            this.set_transition( s, k, this.t.length-2, r );
+            var rprime = this.new_node();
+            this.set_transition( r, this.t.length-1, infinity, rprime );
+        }
+        this.final_states[r] = string_num;
+    }
+
+    find_next( t, start = 0 ) {
+        var s = this.root;
+        var i = start;
+        outer: while( i < t.length ) {
+            var [k, p, sprime] = this.get_transition( s, t[i] );
+            var iprime = i;
+            var kprime = k;
+            while( kprime <= p ) {
+                if( p == infinity || this.t[kprime] != t[iprime] ) {
+                    if( s == this.root ) {
+                        i++;
+                    } else {
+                        s = this.fprime[s];
+                    }
+                    continue outer;
+                }
+                kprime++;
+                iprime++;
+            }
+            s = sprime;
+            i += p - k + 1;
+            if( this.final_states.hasOwnProperty(s) ) {
+                var string_num = this.final_states[s];
+                return [i-this.string_lengths[string_num], i, string_num];
+            }
+        }
+        return [i, i, 0];
     }
 }
-
-
