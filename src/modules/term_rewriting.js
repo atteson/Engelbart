5d6a7e2f1b6837cf637f58fea96f0df8b1c57eee
BuildTools <unconfigured@null.spigotmc.org>
2022-06-11 12:14:12 -0400

    commit before change

--- src/modules/term_rewriting.js
+++ src/modules/term_rewriting.js
@@ -2,7 +2,9 @@
 // operators are in the second list
 
 var variables = [];
-var operators = [];
+var variable_tokens = new Map();
+var nonterminals = new Map();
+var production_rules = [];
 var rules = [];
 
 import { SuffixTree } from "../modules/suffix_tree.js";
@@ -17,14 +19,12 @@ function addVariable(event) {
         newvariable.value = "";
 
         variables.push( variable );
-
+        variable_tokens[tokenizer.push_string( variable )] = variable;
+    
         var variablesmath = document.getElementById("variablesmath");
         MathJax.typesetClear([variablesmath]);
         variablesmath.innerHTML = "$$" + variables.join(",") + "$$";
-        MathJax.typeset([variablesmath]);
-
-        //tokenizer.push( variable );
-    }
+        MathJax.typeset([variablesmath]);}
 }
 
 document.getElementById("newvariable").addEventListener("keyup",addVariable);
@@ -32,14 +32,35 @@ document.getElementById("newvariable").addEventListener("keyup",addVariable);
 function addOperator(event) {
     if( event.keyCode == 13 ) {
         var newitem = document.getElementById( "newoperator" );
-        operators.push( newitem.value );
+        var operator = newitem.value;
+
         newitem.value = "";
+
+        var tokens = [];
+        var i = 0;
+        var [start, end, string_num] = tokenizer.find_next_string( operator, i );
+        while( string_num != 0 ) {
+            if( start > i ) {
+                var substring = operator.substring( i, start - 1 );
+                nonterminals[tokens.push_string( substring )] = substring;
+
+            }
+            [start, end, string_num] = tokenizer.find_next_string( operator, i );
+            i = end;
+        }
+        if( end < operator.length ) {
+            var substring = operator.substring( end, operator.length - 1);
+            tokens.push_string( substring ); 
+         }
+
         var table = document.getElementById( "operators" );
         var cell = table.insertRow().insertCell();
         cell.innerHTML = "$$" + operators[operators.length-1] + "$$";
         MathJax.typeset( [cell] );
     }
 }
+
+document.getElementById("newoperator").addEventListener("keyup",addOperator)
 
 function addRule(event) {
     if( event.keyCode == 13 ) {
