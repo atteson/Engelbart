504afbfebd0ae8babfb162191ff96f8650ba5b74
BuildTools <unconfigured@null.spigotmc.org>
2022-06-15 23:42:23 -0400

    first cut of key listener code

--- src/modules/term_rewriting.js
+++ src/modules/term_rewriting.js
@@ -12,49 +12,47 @@ import { SuffixTree } from "../modules/suffix_tree.js";
 import "../submodules/MathJax-src/es5/tex-chtml.js"
 var tokenizer = new SuffixTree( 255 );
 
-function addEventListener( element, event, listener ) {
-    element.addEventListener(event, listener);
-    if( !element.hasOwnProperty("eventListeners")){
-        element.eventListeners = new Map();
+function addKeyListener( element, key, listener ) {
+    if( !element.hasOwnProperty("keyListeners")){
+        element.keyListeners = new Map();
     }
-    element.eventListeners.set( event, listener );
+    element.keyListeners.set( key, listener );
 }
 
-function select(event) {
-    if( !(event.path[0].hasOwnProperty("eventListeners") && event.path[0].eventListeners.has("keyup")) &&
-     active_element != undefined ) {
-        switch(event.keyCode) {
-            case 40:
-                active_element.classList.remove("red");
-                var parent = active_element.parentNode;
-                var i = 0;
-                while(parent.children[i] != active_element) {
-                    i++;
-                }
-                if( i+1 < parent.children.length ) {
-                    active_element = parent.children[i+1];
-                    active_element.classList.add("red");
-                }
-                break;
-            case 38:
-                active_element.classList.remove("red");
-                var parent = active_element.parentNode;
-                var i = 0;
-                while(parent.children[i] != active_element){
-                    i++;
-                }
-                if( i > 1 ) {
-                    active_element = parent.children[i-1];
-                    active_element.classList.add("red");
-                } else {
-                    document.getElementById("operator").focus();
-                }
-                break;
+function checkAndRunKeyListener( element, key ) {
+    if( element.hasOwnProperty( "keyListeners" ) && element.keyListeners.has( key ) ) {
+        element.keyListeners.get( key )();
+        return true;
+    }
+    return false;
+}
+
+function keyListener( event ) {
+    if( !checkAndRunKeyListener( event.target, event.keyCode ) && active_element !== undefined ) {
+        checkAndRunKeyListener( active_element, event.keyCode );
+    }
+}
+
+document.addEventListener("keyup",keyListener);
+
+var focusTagSet = new Set(["INPUT"]);
+
+function activateFunction( from, to ) {
+    return () => {
+        if( focusTagSet.has( from.tagName )) {
+            from.blur();
+        } else {
+            from.classList.remove("red");
+            active_element = undefined;
+        }
+        if( focusTagSet.has( to.tagName )) {
+            to.focus();
+        } else {
+            to.classList.add("red");
+            active_element = to;
         }
     }
 }
-
-addEventListener(document,"keyup",select);
 
 function focus(event) {
     if( active_element !== undefined ) {
@@ -63,96 +61,78 @@ function focus(event) {
     }
 }
 
-addEventListener(document,"focusin",focus);
+document.addEventListener("focusin",focus);
 
-function addVariable(event) {
-    if( event.keyCode == 13 ) {
-        var newvariable = document.getElementById("newvariable");
-        var variable = newvariable.value;
+function addVariable() {
+    var newvariable = document.getElementById("newvariable");
+    var variable = newvariable.value;
 
-        newvariable.value = "";
+    newvariable.value = "";
 
-        variables.set( tokenizer.push_string( variable ), variable );
-    
-        var variablesmath = document.getElementById("variablesmath");
-        MathJax.typesetClear([variablesmath]);
-        variablesmath.innerHTML = "$$" + Array.from(variables.values()).join(",") + "$$";
-        MathJax.typeset([variablesmath]);}
+    variables.set( tokenizer.push_string( variable ), variable );
+
+    var variablesmath = document.getElementById("variablesmath");
+    MathJax.typesetClear([variablesmath]);
+    variablesmath.innerHTML = "$$" + Array.from(variables.values()).join(",") + "$$";
+    MathJax.typeset([variablesmath]);
 }
 
-addEventListener(document.getElementById("newvariable"),"keyup",addVariable);
+addKeyListener(document.getElementById("newvariable"),13,addVariable);
 
-function addOperator(event) {
-    if( event.keyCode == 13 ) {
-        var newitem = document.getElementById( "operator" );
-        var operator = newitem.value;
+function addOperator() {
+    var newitem = document.getElementById( "operator" );
+    var operator = newitem.value;
 
-        newitem.value = "";
+    newitem.value = "";
 
-        var tokens = [];
-        var i = 0;
-        var [start, end, token] = tokenizer.find_next_string( operator, i );
-        while( token != 0 ) {
-            tokens.push(token);
-            if( start > i ) {
-                var substring = operator.substring( i, start - 1 );
-                token = tokenizer.push_string( substring );
-                connectors.set( token, substring );
-                tokens.push(token);
-            }
-            i = end;
-            [start, end, token] = tokenizer.find_next_string( operator, i );
-        }
-        if( end < operator.length ) {
-            var substring = operator.substring( end, operator.length - 1);
-            token = tokenizer.push_string( substring ); 
+    var tokens = [];
+    var i = 0;
+    var [start, end, token] = tokenizer.find_next_string( operator, i );
+    while( token != 0 ) {
+        tokens.push(token);
+        if( start > i ) {
+            var substring = operator.substring( i, start - 1 );
+            token = tokenizer.push_string( substring );
             connectors.set( token, substring );
-            tokens.push( token );
-         }
-
-         production_rules.push( tokens );
-
-        var table = document.getElementById( "operators" ).getElementsByTagName("tbody")[0];
-        var row = document.createElement("tr");
-        table.appendChild(row);
-        row.setAttribute("class","outline");
-
-        var operatorcell = document.createElement("td");
-        row.appendChild(operatorcell);
-        operatorcell.innerHTML = "$$" + operator + "$$";
-        MathJax.typeset( [operatorcell] );
-
-        var associativitycell = document.createElement("td");
-        row.appendChild(associativitycell);
-        associativitycell.innerHTML=`
-         <select>
-            <option value="left">left</option>
-            <option value="right">right</option>
-         </select>
-        `;
-        associativitycell.setAttribute("align","right");
-    } else if (event.keyCode== 40) {
-        var operator = document.getElementById("operator");
-        operator.blur();
-        var rows = document.getElementById("operators").getElementsByTagName("tr");
-        if( rows.length > 1) {
-            rows[1].classList.add("red");
-            active_element = rows[1];
+            tokens.push(token);
         }
+        i = end;
+        [start, end, token] = tokenizer.find_next_string( operator, i );
     }
+    if( end < operator.length ) {
+        var substring = operator.substring( end, operator.length - 1);
+        token = tokenizer.push_string( substring ); 
+        connectors.set( token, substring );
+        tokens.push( token );
+        }
+
+        production_rules.push( tokens );
+
+    var table = document.getElementById( "operators" ).getElementsByTagName("tbody")[0];
+    var row = document.createElement("tr");
+    table.appendChild(row);
+    var l = table.children.length;
+    var neighbor = l == 2 ? newitem : table.children[l-1];
+
+    addKeyListener( neighbor, 40, activateFunction( neighbor, row ));
+    addKeyListener( row, 38, activateFunction( row, neighbor ));       
+
+    row.setAttribute("class","outline");
+
+    var operatorcell = document.createElement("td");
+    row.appendChild(operatorcell);
+    operatorcell.innerHTML = "$$" + operator + "$$";
+    MathJax.typeset( [operatorcell] );
+
+    var associativitycell = document.createElement("td");
+    row.appendChild(associativitycell);
+    associativitycell.innerHTML=`
+        <select>
+        <option value="left">left</option>
+        <option value="right">right</option>
+        </select>
+    `;
+    associativitycell.setAttribute("align","right");
 }
 
-addEventListener(document.getElementById("operator"),"keyup",addOperator);
-
-function addRule(event) {
-    if( event.keyCode == 13 ) {
-        var newitem = document.getElementById( "newrule" );
-        rules.push( newitem.value );
-        newitem.value = "";
-        var table = document.getElementById( "rules" );
-        var cell = table.insertRow().insertCell();
-        cell.innerHTML = "$$" + rules[rules.length-1] + "$$";
-        MathJax.typeset( [cell] );
-    }
-}
-
+addKeyListener(document.getElementById("operator"),13,addOperator);
